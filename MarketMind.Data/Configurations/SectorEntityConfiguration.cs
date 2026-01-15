549a7a06bb906f13729289c893d73ac1a04cebbd
Nikolay Bardarov <152870144+NikolayBardarov22@users.noreply.github.com>
2026-01-15 11:25:04 +0200

    Added Sector seeding data.

--- MarketMind.Data/Configurations/SectorEntityConfiguration.cs
+++ MarketMind.Data/Configurations/SectorEntityConfiguration.cs
@@ -18,6 +18,26 @@ namespace MarketMind.Data.Configurations
             entity.Property(e => e.Name)
                 .IsRequired()
                 .HasMaxLength(SectorNameMaxLength);
+
+            entity.HasData(this.GenerateSeedSectors());
+        }
+        private HashSet<Sector> GenerateSeedSectors()
+        {
+            HashSet<Sector> seedSectors = new HashSet<Sector>()
+            {
+                new Sector { Id = 1, Name = "Energy" },
+                new Sector { Id = 2, Name = "Materials" },
+                new Sector { Id = 3, Name = "Industrials" },
+                new Sector { Id = 4, Name = "Consumer Discretionary" },
+                new Sector { Id = 5, Name = "Consumer Staples" },
+                new Sector { Id = 6, Name = "Health Care" },
+                new Sector { Id = 7, Name = "Financials" },
+                new Sector { Id = 8, Name = "Information Technology" },
+                new Sector { Id = 9, Name = "Communication Services" },
+                new Sector { Id = 10, Name = "Utilities" },
+                new Sector { Id = 11, Name = "Real Estate" }
+            };
+            return seedSectors;
         }
     }
 }
---
+++ MarketMind.Data/Migrations/20260115092239_AddSectorSeeding.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MarketMind.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSectorSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Sectors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Energy" },
+                    { 2, "Materials" },
+                    { 3, "Industrials" },
+                    { 4, "Consumer Discretionary" },
+                    { 5, "Consumer Staples" },
+                    { 6, "Health Care" },
+                    { 7, "Financials" },
+                    { 8, "Information Technology" },
+                    { 9, "Communication Services" },
+                    { 10, "Utilities" },
+                    { 11, "Real Estate" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Sectors",
+                keyColumn: "Id",
+                keyValue: 11);
+        }
+    }
+}
